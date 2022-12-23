041e14923af1d78f20e0902d0f1cd75aa9efdf2d
Владислав Маковский <alectorkinga@gmail.com>
2022-12-23 12:58:48 +0200

    add:env:uppercase

--- myApp.js
+++ myApp.js
@@ -1,46 +1,15 @@
-let express = require('express');
+require('dotenv').config();
+let express = require("express");
 let app = express();
-console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-  })
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
 app.get("/json", (req, res) => {
-    res.json({"message": "Hello json"})
-})
+  process.env.MESSAGE_STYLE === "uppercase"
+    ? res.json({ message: "HELLO JSON" })
+    : res.json({ message: "Hello json" });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
