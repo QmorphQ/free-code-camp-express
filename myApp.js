b17fdcd913b60eb36c851c5d377d7972e0869e2a
Владислав Маковский <alectorkinga@gmail.com>
2022-12-23 14:59:53 +0200

    add:middleware:console.log

--- myApp.js
+++ myApp.js
@@ -1,13 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
 app.use("/", (req, res, next) => {
-console.log(req.method + " " + req.path + " - " + req.ip);
-next();
+  console.log(req.method + " " + req.path + " - " + req.ip + " " + req.time);
+  next();
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 app.get("/json", (req, res) => {
   process.env.MESSAGE_STYLE === "uppercase"
